2152015b5781c6f1777dcaedfde8a26393e8e598
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-07-08 12:24:05 +0700

    feat : setup c# and hello c#

--- hello.sh
+++ hello.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
-# Grouping the entire script execution to capture all time outputs
 {
   echo "build times ts"
   time tsc ./ts/Hello.ts --outDir ./execution
   echo "execution runtime"
   time node ./execution/Hello.js
 
-  echo "build times Java"
+  echo "build times java"
   time javac -d ./execution ./java/Hello.java
   echo "execution runtime"
   time java -cp ./execution Hello
 
-  echo "build times C"
-  time javac -d ./execution ./java/Hello.java
+  echo "build times C#"
+  time dotnet build ./csharp/csharp.csproj -o ./execution/hello
   echo "execution runtime"
-  time java -cp ./execution Hello
-
-    echo "build times C"
-  time gcc ./c/hello.c -o ./execution/helloc
-  echo "execution runtime"
-  time ./execution/helloc
+  time ./execution/hello/hello
 
   echo "build times go"
   time go build -o ./execution/hellogo ./go/Hello.go
