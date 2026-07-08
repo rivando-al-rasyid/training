094a3a8dc37467eb50bfbfc0476f2cf2a9e2f353
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-07-08 11:24:32 +0700

    remove white space and add hello.java to hello.sh fix fmt error

--- hello.sh
+++ hello.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
+# Grouping the entire script execution to capture all time outputs
+{
+  echo "build times ts"
+  time tsc ./ts/Hello.ts --outDir ./execution
+  echo "execution runtime"
+  time node ./execution/Hello.js
 
-echo "build times ts"
-time tsc ./ts/hello.ts --outDir ./execution
-echo "execution runtime"
-time node ./execution/hello.js
+  echo "build times Java"
+  time javac -d ./execution ./java/Hello.java
+  echo "execution runtime"
+  time java -cp ./execution Hello
 
+  echo "build times C"
+  time javac -d ./execution ./java/Hello.java
+  echo "execution runtime"
+  time java -cp ./execution Hello
+
+    echo "build times C"
+  time gcc ./c/hello.c -o ./execution/helloc
+  echo "execution runtime"
+  time ./execution/helloc
+
+  echo "build times go"
+  time go build -o ./execution/hellogo ./go/Hello.go
+  echo "execution runtime"
+  time ./execution/hellogo
+
+} 2>&1
--- java/Hello.java
+++ java/Hello.java
@@ -3,4 +3,3 @@ public class Hello {
         System.out.println("hello from java");
     }
 }
-
