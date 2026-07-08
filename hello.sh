181c3fc306c295da94a081b76683cc8b62e51251
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-07-08 12:51:09 +0700

    ref : move c# to above

--- hello.sh
+++ hello.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 {
+  echo "build times C#"
+  time dotnet build ./csharp/csharp.csproj -o ./execution/hello
+  echo "execution runtime"
+  time ./execution/hello/csharp
+
   echo "build times ts"
   time tsc ./ts/Hello.ts --outDir ./execution
   echo "execution runtime"
@@ -11,10 +16,6 @@
   echo "execution runtime"
   time java -cp ./execution Hello
 
-  echo "build times C#"
-  time dotnet build ./csharp/csharp.csproj -o ./execution/hello
-  echo "execution runtime"
-  time ./execution/hello/csharp
 
   echo "build times go"
   time go build -o ./execution/hellogo ./go/Hello.go
